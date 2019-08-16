c4659cff1a22af9796690b9e75fd5dda7d29505a
Mahi22 <mahesh.masalkar22@gmail.com>
2019-08-16 14:59:00 +0530

    Added basic two actions for test

--- src/actions.ts
+++ src/actions.ts
@@ -1,3 +1,42 @@
-const Actions = 'MWS_FUNCTION_TREE_ACTIONS';
+import { compose, tail, split } from 'ramda';
+import { NodeJSMWSClient as MWSClient } from './nodejs';
+
+let Actions;
+
+/**
+ * @param credentials - { marketplace, accesskey, secret, sellerID, mwsAuthToken }
+ * @return authfetch
+ */
+Actions.createAmazonAuthfetch = async ({ props: { credentials } }) => ({
+  authfetch: new MWSClient(
+    compose(
+      tail,
+      split(' ')
+    )(credentials.marketplace),
+    credentials.appId,
+    credentials.appSecret,
+    credentials.sellerId,
+    credentials.authToken
+  )
+});
+
+/**
+ * @param authfetch
+ */
+Actions.checkOrderServiceStatus = async ({ props: { authfetch } }) =>
+new Promise((resolve, reject) => {
+  authfetch.ListMarketplaceParticipations(function(err, res) {
+    if (err) {
+      reject(err);
+    } else {
+      if (res.status === 200) {
+        resolve({ valid: true });
+      } else {
+        console.log(res);
+        resolve({ valid: false, res });
+      }
+    }
+  });
+});
 
 module.exports = Actions;

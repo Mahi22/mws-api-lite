c45880727499282f54af7e0bf419c0987fe25387
Mahi22 <mahesh.masalkar22@gmail.com>
2019-08-16 16:43:22 +0530

    Updated gitignore for credentials file

--- credentials.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-  marketplace: 'AMAZON IN',
-  appId: 'AKIAIT3CXSSSWQF5PRGQ',
-  appSecret: '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',
-  sellerId: 'A3AUC4UG0LX5TC',
-  authToken: ''
-};
